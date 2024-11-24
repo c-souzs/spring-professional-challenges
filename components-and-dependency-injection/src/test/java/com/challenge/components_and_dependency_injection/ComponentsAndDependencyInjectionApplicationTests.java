d77f9fcd70886e270652cdda2c8ef111512396b6
Caio Souza <souzsdev@gmail.com>
2024-11-24 14:01:09 -0300

    settings-challenge-01
    Configuracao do desafio 01

---
+++ components-and-dependency-injection/src/test/java/com/challenge/components_and_dependency_injection/ComponentsAndDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.components_and_dependency_injection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentsAndDependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
