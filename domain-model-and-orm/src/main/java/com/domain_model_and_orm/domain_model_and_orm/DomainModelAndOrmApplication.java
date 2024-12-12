6eb2d54d2d60210215d91a125037fcb3768c6e09
Caio Souza <souzsdev@gmail.com>
2024-12-12 10:45:10 -0300

    impl-challenge-02
    Implementacao do desafio 02

---
+++ domain-model-and-orm/src/main/java/com/domain_model_and_orm/domain_model_and_orm/DomainModelAndOrmApplication.java
@@ -0,0 +1,13 @@
+package com.domain_model_and_orm.domain_model_and_orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainModelAndOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainModelAndOrmApplication.class, args);
+	}
+
+}
---
+++ domain-model-and-orm/src/test/java/com/domain_model_and_orm/domain_model_and_orm/DomainModelAndOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain_model_and_orm.domain_model_and_orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainModelAndOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
